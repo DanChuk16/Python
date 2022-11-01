663d983fb6668207c1559a3a18b2d704a0e89900
DanChuk16 <daniilcuhrij@gmail.com>
2022-11-01 20:13:43 +0200

    update hm2 ex 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 number1 = int(input('number1->'))
 number2 = int(input('number2->'))
 number3 = int(input('number3->'))
-print('Number->', number1, number2, number3)
+print('Number->', int((number1 * 100) + (number2 * 10) + number3))
