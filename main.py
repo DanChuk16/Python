368f81fe4fc560bbfe7c3ef07942cfa818fe31be
DanChuk16 <daniilcuhrij@gmail.com>
2022-11-03 15:51:19 +0200

    update hm2 ex 5 (2 way)

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ while number_1 > 0:
     number_2 = number_2 * 10
     number_2 = number_2 + digit
 print('Number_2 =', number_2)
+
+number_1 = int(input('Number_1 = '))
+a = int(number_1 % 10)
+a1 = int(number_1 // 10)
+b = int(a1 % 10)
+b1 = int(a1 // 10)
+c = int(b1 % 10)
+d = int(b1 // 10)
+print('Number_2 =', int((a * 1000) + (b * 100) + (c * 10) + d))
