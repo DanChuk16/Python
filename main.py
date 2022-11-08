00edf5bf01f185fe8bbe158e937a57b4900fb9b4
DanChuk16 <daniilcuhrij@gmail.com>
2022-11-08 20:30:24 +0200

    update lab2 ex3

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-number_1 = int(input('Number 1 = '))
-number_2 = int(input('Number 2 = '))
-
-if number_1 >= number_2:
+number_1 = float(input('Number 1 = '))
+number_2 = float(input('Number 2 = '))
+if number_1 > number_2:
     print(number_1)
-elif number_1 <= number_2:
+elif number_1 < number_2:
     print(number_2)
+else:
+    print('All values is equal!')
