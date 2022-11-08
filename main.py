87cbc845be82e0fac2bbee5ef1a03c85fb7c765c
DanChuk16 <daniilcuhrij@gmail.com>
2022-11-08 20:40:11 +0200

    update hm3 ex1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ number_3 = float(input('Number 3 = '))
 print('#------------------<Menu>------------------#')
 print(f'|   + : Show sum for {number_1}, {number_2} and {number_3}      |')
 print(f'|   * : Show product for {number_1}, {number_2} and {number_3}  |')
-print('#----------------------------------------#')
+print('#------------------------------------------#')
 action = input('Actoin: ')
 if action == '+':
     print(f'{number_1} + {number_2} + {number_3} = {number_1 + number_2 + number_3}')
