5c38f5e56a2c699240fa539b371618bcf03225d0
DanChuk16 <daniilcuhrij@gmail.com>
2022-11-08 17:48:08 +0200

    update hm3 ex 1

---
+++ main.py
@@ -0,0 +1,14 @@
+number_1 = float(input('Number 1 = '))
+number_2 = float(input('Number 2 = '))
+number_3 = float(input('Number 3 = '))
+print('#---------------<Menu>---------------#')
+print(f'|   + : Show sum for {number_1}, {number_2} and {number_3}      |')
+print(f'|   * : Show product for {number_1}, {number_2} and {number_3}  |')
+print('#------------------------------------#')
+action = input('Actoin: ')
+if action == '+':
+    print(f'{number_1} + {number_2} + {number_3} = {number_1 + number_2 + number_3}')
+elif action == '*':
+    print(f'{number_1} * {number_2} * {number_3} = {number_1 * number_2 * number_3}')
+else:
+    print('Command not found!')
