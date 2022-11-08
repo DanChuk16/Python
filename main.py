2b240c91030fd229c956dbaaf66758800a6971b8
DanChuk16 <daniilcuhrij@gmail.com>
2022-11-08 22:20:33 +0200

    update hm2 ex5

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 number_1 = int(input('Number_1 = '))
-number_2 = 0
-while number_1 > 0:
-    digit = number_1 % 10
-    number_1 = number_1 // 10
-    number_2 = number_2 * 10
-    number_2 = number_2 + digit
-print('Number_2 =', number_2)
-
-number_1 = int(input('Number_1 = '))
 a = int(number_1 % 10)
 a1 = int(number_1 // 10)
 b = int(a1 % 10)
