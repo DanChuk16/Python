890cbb0d5114f6fb4b34972682ed44e6eff9f7fc
DanChuk16 <daniilcuhrij@gmail.com>
2022-11-11 01:03:06 +0200

    update lab3 ex4

---
+++ main.py
@@ -0,0 +1,25 @@
+try:
+    size = float(input('Size (Gb): '))
+    speed = float(input('Speed (Bit/s): '))
+    print('#------------------------------>MENU<------------------------------#')
+    print('|   h : Show how many hours it will take to download the file      |')
+    print('|   m : Show how many minutes it will take to download the file    |')
+    print('|   s : Show how many seconds it will take to download the file    |')
+    print('#------------------------------------------------------------------#')
+    action = input('Action: ')
+    bits = float(size * 8589934592)
+    time_sec = float(bits / speed)
+    if action == 'h':
+        print(int(time_sec / 3600), 'hours to download the file')
+        # print(f'{time_sec / 3600} hours to download the file')
+    elif action == 'm':
+        print(int(time_sec / 60), 'minutes to download the file')
+        # print(f'{time_sec / 60} minutes to download the file')
+    elif action == 's':
+        print(int(time_sec), 'seconds to download the file')
+        # print(f'{time_sec} seconds to download the file')
+    else:
+        print(int(time_sec / 3600), ':', int((time_sec % 3600) / 60), ':', int((time_sec % 3600) % 60), 'to download the file')
+        #print(f'{(time_sec / 3600)}:{((time_sec % 3600) / 60)}:{(time_sec % 3600) % 60} to download the file')
+except:
+    print('You typed something wrong!')
