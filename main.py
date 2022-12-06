6ff339521025bd2b115306916461a0dfac84c785
DanChuk16 <daniilcuhrij@gmail.com>
2022-12-06 16:29:38 +0200

    update lab4 ex 2

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-begin = int(input('Begin:'))
-end = int(input('End:'))
+try:
+    begin = int(input('Begin:'))
+    end = int(input('End:'))
 
-for item in range(begin, end+1):
-    print(item, end='\t')
+    for item in range(begin, end + 1):
+        if item % 2 == 0:
+            print(item, end='\t')
+except Exception as ex:
+    print(f'Error: {ex}')
