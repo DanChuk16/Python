d1ba90a6e9f3412875dffa4987b4540a80eb90a5
DanChuk16 <daniilcuhrij@gmail.com>
2022-12-06 22:12:15 +0200

    update lab4 ex 2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ try:
     end = int(input('End:'))
 
     for item in range(begin, end + 1):
-        if item % 2 == 0:
+        if item % 2 != 0:
             print(item, end='\t')
 except Exception as ex:
     print(f'Error: {ex}')
