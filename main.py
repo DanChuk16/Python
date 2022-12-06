7a8748f77cf9ab39b19447bb998f784c6358ddce
DanChuk16 <daniilcuhrij@gmail.com>
2022-12-06 18:34:07 +0200

    update hm6 ex 3

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ try:
     end = int(input('End:'))
 
     for item in range(begin, end + 1):
-        print(item, end='\t')
+        print(item, end=' ')
 
     for item in reversed(range(begin, end + 1)):
-        print(item, end='\t')
+        print(item, end=' ')
 
     for item in range(begin, end + 1):
         if item % 7 == 0:
-            print(item, end='\t')
+            print(item, end=' ')
 
     for item in range(begin, end + 1):
         if item % 5 == 0:
+
 
 except Exception as ex:
     print(f'Error: {ex}')
