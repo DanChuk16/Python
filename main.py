b782c2d2d77912d85a61f483a541fb9dbf263790
DanChuk16 <daniilcuhrij@gmail.com>
2022-12-06 16:25:57 +0200

    update lab4 ex 1

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ try:
     for item in range(begin, end + 1):
         print(item, end='\t')
 
-except:
-    print('You entered the wrong number!')
+except ValueError as vl_ex:
+    print(f'Value Error: {vl_ex}')
+except Exception as ex:
+    print(f'Error: {ex}')
