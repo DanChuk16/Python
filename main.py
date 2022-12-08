9e1a2a657c33c2c310fce4843a391a7078bff939
DanChuk16 <daniilcuhrij@gmail.com>
2022-12-08 15:22:00 +0200

    update hm6 ex 2

--- main.py
+++ main.py
@@ -4,16 +4,24 @@ try:
 
     for item in range(begin, end + 1):
         print(item, end=' ')
+    print()
+
 
     for item in reversed(range(begin, end + 1)):
         print(item, end=' ')
+    print()
+
 
     for item in range(begin, end + 1):
         if item % 7 == 0:
             print(item, end=' ')
+    print()
 
+    counter = 0
     for item in range(begin, end + 1):
         if item % 5 == 0:
+            counter += 1
+    print(counter)
 
 
 except Exception as ex:
