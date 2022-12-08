456c5579ac8e2d18eda15c5cd957a55846ddff41
DanChuk16 <daniilcuhrij@gmail.com>
2022-12-08 16:08:23 +0200

    update lab5 ex 4

---
+++ main.py
@@ -0,0 +1,9 @@
+try:
+    number = int(input('Number: '))
+    symbol = input('Symbol: ')
+    for i in range(1, number + 1):
+        print(symbol, end= ' ')
+
+except Exception as ex:
+    print(f'Error: {ex}')
+    print(f'Name: {ex.__class__.__name__}')
