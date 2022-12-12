ae25dfddad6abefce2d303d948de9f6b6255d2ac
DanChuk16 <daniilcuhrij@gmail.com>
2022-12-12 17:25:18 +0200

    update hm7 ex 4

---
+++ main.py
@@ -0,0 +1,26 @@
+try:
+    number = 0
+    sum = 0
+    min = 0
+    max = 0
+    trigger = True
+    while True:
+        number = int(input('Number: '))
+        if number == 7:
+            break
+        else:
+            sum += number
+            if trigger:
+                min = max = number
+                trigger = False
+            else:
+                if max < number:
+                    max = number
+                if min > number:
+                    min = number
+    print(f'Sum: {sum}')
+    print(f'Max: {max}')
+    print(f'Min: {min}')
+
+except Exception as ex:
+    print(ex)
