c7062ef81a7f683b984bc3c5415db9c2c9ae1c62
DanChuk16 <daniilcuhrij@gmail.com>
2022-12-20 17:22:50 +0200

    update lab 6 ex 3

---
+++ main.py
@@ -0,0 +1,19 @@
+try:
+    begin = int(input('Початок: '))
+    end = int(input('Кінець: '))
+    if begin > end:
+        begin, end = end, begin
+    while True:
+        number = int(input('Число: '))
+        if begin <= number <= end:
+            for i in range(begin, end+1):
+                if i < number or i > number:
+                    print(i, end=" ")
+                else:
+                    print(f"!{i}!", end=" ")
+            break
+        else:
+            print('Помилка: Число не входить в зазначені діапазони. Повтор операції.')
+
+except Exception as ex:
+    print(f'Error [{ex.__class__.__name__}]: {ex}')
