169e8f042a0ed27c34ad6bb56e0c2162a3193cd0
DanChuk16 <daniilcuhrij@gmail.com>
2022-12-21 23:35:41 +0200

    update hm8 ex 2

---
+++ main.py
@@ -0,0 +1,16 @@
+try:
+    start = int(100)
+    end = int(999)
+    counter = 0
+    for i in range(start, end + 1):
+        a = (i // 100) % 10
+        b = (i // 10) % 10
+        c = i % 10
+        if a == b or a == c or b == c:
+            counter += 1
+    print(f'Start: {start}\nEnd: {end}')
+    print(f'Integers that have two identical digits: {counter}')
+
+except Exception as ex:
+    print(f'Error: {ex}')
+    print(f'Name: {ex.__class__.__name__}')
