01cc36c2caa2fbb2cf0cd319f1f25309ac80deb5
DanChuk16 <daniilcuhrij@gmail.com>
2023-01-10 17:36:59 +0200

    update lab7 ex3

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 try:
     size = int(input('Size: '))
-    for i in range(1, size + 1):
-        for a in range(1, size + 1):
-            if i == 0:
-                print('*', end=' ')
+    for i in range(0, size):
+        for a in range(0, size):
+            if i == 0 or i == size-1:
+                print('*', end='  ')
             else:
-                if a == 0:
-                    print('*', end=' ')
+                if a == 0 or a == size-1:
+                    print('*', end='  ')
                 else:
-                    print(' ', end=' ')
+                    print(' ', end='  ')
         print()
 except Exception as ex:
     print(f'Error: [{ex.__class__.__name__}]: {ex}')
