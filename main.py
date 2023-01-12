3042eddd62a18170a86a5454be63317a42a6ff8b
DanChuk16 <daniilcuhrij@gmail.com>
2023-01-12 16:52:24 +0200

    update hm10 ex1

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ try:
     end = int(input('End: '))
     if start > end:
         start, end = end, start
+    print('Prime numbers:', end=' ')
     for number in range(start, end + 1):
         counter = 0
         for i in range(1, number + 1):
-            if number % 1 == 0:
+            if number % i == 0:
                 counter += 1
         if counter == 2:
-            print('Prime numbers:', number, end=' ')
+            print(number, end=' ')
     print()
 
 except Exception as ex:
