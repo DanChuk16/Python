46d6831eb32eeb3dc31b55ea87dad676f9719346
DanChuk16 <daniilcuhrij@gmail.com>
2023-01-12 11:45:36 +0200

    update hm10 ex1

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 start = int(input('Start: '))
 end = int(input('End: '))
+counter = 0
 if start > end:
-        start, end = end, start
+    start, end = end, start
 for i in range(start, end + 1):
-    if i % 1 == 0 and i % i == 0:
+    if i % 1 == 0 and i % i == 0: #and i % 2 != 0 and i % 3 != 0 and i % 5 != 0 and i % 7 != 0 and i % 9 != 0:
         print(f'Prime numbers: {i}')
