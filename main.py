067cbe818e7b6722c0193dd0e0dc993f2a70bc14
DanChuk16 <daniilcuhrij@gmail.com>
2023-01-29 22:50:13 +0200

    update lab 9 ex 3

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 try:
     num = input('Numbers: ').split()
     Sum = 0
+    count = 0
     for i in num:
         if i.isnumeric():
             Sum += i
-    print(Sum)
+            count += 1
+    print(f'Sum: {Sum}')
+    print(f'Avg: {Sum/count}')
 
 except Exception as ex:
     print(f'Error: {ex}')
