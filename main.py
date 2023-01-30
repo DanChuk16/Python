7806c41674e46c34065aae8dc02d1cdf451201f9
DanChuk16 <daniilcuhrij@gmail.com>
2023-01-30 16:30:03 +0200

    hm 12 ex 1

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ try:
     whirlpool = input('Whirlpool: ')
     whirlpool_2 = whirlpool
     whirlpool = list(whirlpool)
-    print(whirlpool)
     for i in whirlpool:
         if '+' in i:
             whirlpool_2 = whirlpool_2.split('+')
-            res = int(whirlpool_2.index(0) + whirlpool_2.index(1))
-            #index_plus = whirlpool.index('+')
-            #num_1 = whirlpool[whirlpool.index(min(whirlpool))-1: index_plus]
-        if '-' in whirlpool:
-            index_minus = whirlpool.index('-')
-        if '*' in whirlpool:
-            index_mul = whirlpool.index('*')
-        if '/' in whirlpool:
-            index_divisi = whirlpool.index('/')
+            res = int(int(whirlpool_2[0]) + int(whirlpool_2[1]))
+            print(res)
+        if '-' in i:
+            whirlpool_2 = whirlpool_2.split('-')
+            res = int(int(whirlpool_2[0]) - int(whirlpool_2[1]))
+            print(res)
+        if '*' in i:
+            whirlpool_2 = whirlpool_2.split('*')
+            res = int(int(whirlpool_2[0]) * int(whirlpool_2[1]))
+            print(res)
+        if '/' in i:
+            whirlpool_2 = whirlpool_2.split('/')
+            res = float(float(whirlpool_2[0]) / float(whirlpool_2[1]))
+            print(res)
+
 except Exception as ex:
     print(f'Error: [{ex.__class__.__name__}]: {ex}')
