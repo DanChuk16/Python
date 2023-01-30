f9007aadb53d6b43928bb6c3f58d377965375024
DanChuk16 <daniilcuhrij@gmail.com>
2023-01-30 12:25:46 +0200

    hm12 ex 1

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ try:
     whirlpool = input('Whirlpool: ')
     whirlpool = list(whirlpool)
     print(whirlpool)
+    for i in whirlpool:
+        if '+' in i:
+            index_plus = whirlpool.index('+')
 
+        elif '-' in whirlpool:
+            index_minus = whirlpool.index('-')
+        elif '*' in whirlpool:
+            index_mul = whirlpool.index('*')
+        elif '/' in whirlpool:
+            index_divisi = whirlpool.index('/')
 
 except Exception as ex:
     print(f'Error: [{ex.__class__.__name__}]: {ex}')
