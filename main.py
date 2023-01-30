7442640e1144e838ca38039bd64f3620a5f7def5
DanChuk16 <daniilcuhrij@gmail.com>
2023-01-30 15:36:38 +0200

    hm 12 ex 1

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 try:
     whirlpool = input('Whirlpool: ')
-    while True:
-        whirlpool = list(whirlpool)
-        print(whirlpool)
-        for i in whirlpool:
-            if '+' in i:
-                index_plus = whirlpool.index('+')
-                num_1 = whirlpool[whirlpool.index(min(whirlpool))-1: index_plus]
-            if '-' in whirlpool:
-                index_minus = whirlpool.index('-')
-            if '*' in whirlpool:
-                index_mul = whirlpool.index('*')
-            if '/' in whirlpool:
-                index_divisi = whirlpool.index('/')
-        break
+    whirlpool_2 = whirlpool
+    whirlpool = list(whirlpool)
+    print(whirlpool)
+    for i in whirlpool:
+        if '+' in i:
+            whirlpool_2 = whirlpool_2.split('+')
+            res = int(whirlpool_2.index(0) + whirlpool_2.index(1))
+            #index_plus = whirlpool.index('+')
+            #num_1 = whirlpool[whirlpool.index(min(whirlpool))-1: index_plus]
+        if '-' in whirlpool:
+            index_minus = whirlpool.index('-')
+        if '*' in whirlpool:
+            index_mul = whirlpool.index('*')
+        if '/' in whirlpool:
+            index_divisi = whirlpool.index('/')
 except Exception as ex:
     print(f'Error: [{ex.__class__.__name__}]: {ex}')
