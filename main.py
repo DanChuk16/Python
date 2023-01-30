e7e2c6fd1367a995b4294204c0c7abed43f82052
DanChuk16 <daniilcuhrij@gmail.com>
2023-01-30 12:59:16 +0200

    hm12 ex 1

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 try:
     whirlpool = input('Whirlpool: ')
-    whirlpool = list(whirlpool)
-    print(whirlpool)
-    for i in whirlpool:
-        if '+' in i:
-            index_plus = whirlpool.index('+')
-            num_1 = whirlpool[whirlpool.index(min(whirlpool)): index_plus].copy()
-
-        if '-' in whirlpool:
-            index_minus = whirlpool.index('-')
-        if '*' in whirlpool:
-            index_mul = whirlpool.index('*')
-        if '/' in whirlpool:
-            index_divisi = whirlpool.index('/')
+    while True:
+        whirlpool = list(whirlpool)
+        print(whirlpool)
+        for i in whirlpool:
+            if '+' in i:
+                index_plus = whirlpool.index('+')
+                num_1 = whirlpool[whirlpool.index(min(whirlpool))-1: index_plus]
+            if '-' in whirlpool:
+                index_minus = whirlpool.index('-')
+            if '*' in whirlpool:
+                index_mul = whirlpool.index('*')
+            if '/' in whirlpool:
+                index_divisi = whirlpool.index('/')
+        break
 except Exception as ex:
     print(f'Error: [{ex.__class__.__name__}]: {ex}')
