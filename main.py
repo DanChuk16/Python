29bf27a5e6f2bc4b176d39b16d0d74c4541c459f
DanChuk16 <daniilcuhrij@gmail.com>
2023-01-30 12:38:52 +0200

    hm12 ex 2

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ try:
     for i in whirlpool:
         if '+' in i:
             index_plus = whirlpool.index('+')
-            num_1 = whirlpool[whirlpool.index(min(whirlpool): index_plus)]
+            num_1 = whirlpool[whirlpool.index(min(whirlpool)): index_plus].copy()
+
         if '-' in whirlpool:
             index_minus = whirlpool.index('-')
         if '*' in whirlpool:
             index_mul = whirlpool.index('*')
         if '/' in whirlpool:
             index_divisi = whirlpool.index('/')
-
 except Exception as ex:
     print(f'Error: [{ex.__class__.__name__}]: {ex}')
