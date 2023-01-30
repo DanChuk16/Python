aa493edae41aa06403661839ee462befd99295c7
DanChuk16 <daniilcuhrij@gmail.com>
2023-01-30 20:40:11 +0200

    hm 13

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ try:
         print(f'3) Elements common to the two lists: {list3}')
         list3 = list(set(list1) ^ set(list2))
         print(f'4) Only the unique elements of each of the lists: {list3}')
-        list3 =
+        list3 = [min(list1), max(list1), min(list2), max(list2)]
+        print(f'5) Only the minimum and maximum value of each of lists: {list3}')
     else:
         print('Items in lists must be positive!')
 except Exception as ex:
