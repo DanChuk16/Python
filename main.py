048e63470e662743bb7220817bb643d047064a4d
DanChuk16 <daniilcuhrij@gmail.com>
2023-01-31 23:45:57 +0200

    lab 10 ex 1 and 2

--- main.py
+++ main.py
@@ -28,19 +28,18 @@ try:
     for a in range(0, lengthOfFList):
         if a % 3 == 0:
             counter_4 *= numbers[a]
-    for e in range(0, lengthOfFList + 1):
-        if numbers[e] > 0:
-            start_index = e
-    for e in range(lengthOfFList, -1, -1):
+    for item in numbers:
+        if item > 0:
+            start_index = numbers.index(item)
+            break
+    for e in range(lengthOfFList-1, -1, -1):
         if numbers[e] > 0:
             end_index = e
-    partList_2 = numbers[numbers.index(start_index)+1: numbers.index(end_index)].copy()
+            break
+    partList_2 = numbers[start_index+1: end_index].copy()
     Sum = 0
     for it in partList_2:
         Sum += it
-    print(Sum)
-    #print(start_index)
-    #print(end_index)
 
     partList = numbers[numbers.index(max(numbers))+1: numbers.index(min(numbers))].copy()
     mul = 1
@@ -52,7 +51,7 @@ try:
     print(f'Sum odd numbers: {counter_3}')
     print(f'Product of numbers that are multiples of 3: {counter_4}')
     print(f'The product of elements between the minimum and maximum element: {mul}')
-    print()
+    print(f'The sum of elements between the first and last positive element: {Sum}')
     print()
 
     print('Even numbers:')
